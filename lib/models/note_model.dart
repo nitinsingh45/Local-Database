67988cca8609d9009df4c295b5c67e3b5b9a556e
NITIN SINGH <nitin2201singh@gmail.com>
2024-01-24 20:29:38 +0530

    first commit

---
+++ lib/models/note_model.dart
@@ -0,0 +1,12 @@
+class Note {
+  final int? id;
+  final String title;
+  final String description;
+
+  const Note({required this.title, required this.description, this.id});
+
+  factory Note.fromJson(Map<String, dynamic> json) => Note(
+      id: json['id'], title: json['title'], description: json['description']);
+  Map<String, dynamic> toJason() =>
+      {'id': id, 'title': title, 'description': description};
+}
